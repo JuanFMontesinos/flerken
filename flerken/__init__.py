059f55263bd401c71aaf46ead0b96af23ab0c385
Juan Montesinos <jf95.zgz@gmail.com>
2019-03-28 16:21:40 +0100

    Setup file bug error + cuda timing
    Fixed setup_file bug error and added a decorator to time cuda functions.

--- flerken/__init__.py
+++ flerken/__init__.py
@@ -1,3 +1,16 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+import torch
 from .framework.pytorchframework import pytorchfw
+
+def cuda_timing(func):
+    def inner(*args,**kwargs):
+        start = torch.cuda.Event(enable_timing=True)
+        end = torch.cuda.Event(enable_timing=True)
+        start.record()
+        output = func(*args,**kwargs)
+        end.record()
+        torch.cuda.synchronize()
+        time = start.elapsed_time(end)
+        return output,time
+    return inner
