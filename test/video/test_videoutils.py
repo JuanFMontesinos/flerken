6457b3416e47ef4e454d6ea784cef64ed6fb5473
Juan Montesinos <jf95.zgz@gmail.com>
2019-06-01 20:02:30 +0200

    unittest issue

--- test/video/test_videoutils.py
+++ test/video/test_videoutils.py
@@ -5,15 +5,15 @@ from flerken.video import utils
 
 class TestFramework(unittest.TestCase):
     def test_get_metadata(self):
-        time, fps = utils.get_video_metadata('./test/test.mp4', display='s')
+        time, fps = utils.get_duration_fps('./test/test.mp4', display='s')
         self.assertEqual(fps, 25.0)
         self.assertEqual(time, 18.65)
 
-        time, fps = utils.get_video_metadata('./test/test.mp4', display='ms')
+        time, fps = utils.get_duration_fps('./test/test.mp4', display='ms')
         self.assertEqual(time, 18650.0)
 
-        time, fps = utils.get_video_metadata('./test/test.mp4', display='min')
+        time, fps = utils.get_duration_fps('./test/test.mp4', display='min')
         self.assertEqual(time, 18.65 / 60)
 
-        time, fps = utils.get_video_metadata('./test/test.mp4', display='h')
+        time, fps = utils.get_duration_fps('./test/test.mp4', display='h')
         self.assertEqual(time, 18.65 / 3600)
